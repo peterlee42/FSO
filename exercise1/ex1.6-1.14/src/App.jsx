9582465da929eca45bd0efa2a081826fe831b3c5
Peter Lee <76502486+peterlee42@users.noreply.github.com>
2025-03-15 00:43:28 -0400

    finished ex 1.6-1.1

---
+++ exercise1/ex1.6-1.14/src/App.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return(
+      <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text} </td>
+      <td>{props.value}</td>
+    </tr>
+  )
+}
+
+const Statistics = (props) => {
+  if (props.all != 0) {
+    return (
+      <div>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="all" value={props.all} />
+        <StatisticLine text="average" value={props.average} />
+        <StatisticLine text="positive" value={props.positive + "%"} />
+      </div>
+    )
+  }
+  else {
+    return (
+      <div>No feedback given</div>
+    )
+  }
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+  
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positive = (good / total) * 100
+
+  return (
+    <div>
+      <Header title="give feedback" />
+      <Button handleClick={handleGoodClick} text={"good"} />
+      <Button handleClick={handleNeutralClick} text={"neutral"} />
+      <Button handleClick={handleBadClick} text={"bad"} />
+      <Header title="statistics" />
+      <Statistics good={good} neutral={neutral} bad={bad} all={total} average={average} positive={positive} />
+    </div>
+  )
+}
+
+export default App
