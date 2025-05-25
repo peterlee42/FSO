e30b89528838093e73978c05b018a44e82a23a6d
Peter Lee <76502486+peterlee42@users.noreply.github.com>
2025-05-25 16:59:28 -0400

    ex 2.11

--- exercise2/phonebook/src/App.jsx
+++ exercise2/phonebook/src/App.jsx
@@ -1,31 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Filter from './components/Filter.jsx'
 import PersonForm from './components/PersonForm.jsx'
 import Persons from './components/Persons.jsx'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchName, setSearchName] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (e) => {
     e.preventDefault()
 
     if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase())){
       alert(`${newName} is already added to phonebook`)
-      return
-    }
-
-    if(persons.some(person => person.number === newNumber)){
-      alert(`${newNumber} is already added to phonebook`)
       return
     }
 
