b960e8580be3e3cb81bd08a1233e4c37ed77030f
Peter Lee <76502486+peterlee42@users.noreply.github.com>
2025-06-18 12:38:56 -0400

    created and refactored blog list app

---
+++ ex4/BlogList/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const blogsRouter = require('./controllers/blogs');
+
+const app = express();
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose
+	.connect(config.MONGODB_URI)
+	.then(() => {
+		logger.info('connected to MongoDB');
+	})
+	.catch((err) => {
+		logger.error('error connecting to MongoDB', err.message);
+	});
+
+app.use(express.json());
+
+app.use('/api', blogsRouter);
+
+module.exports = app;
---
+++ ex4/BlogList/controllers/blogs.js
@@ -0,0 +1,18 @@
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
+
+blogsRouter.get('/blogs', (req, res, next) => {
+	Blog.find({}).then((blogs) => {
+		res.json(blogs);
+	});
+});
+
+blogsRouter.post('/blogs', (req, res) => {
+	const blog = new Blog(req.body);
+
+	blog.save().then((result) => {
+		res.status(201).json(result);
+	});
+});
+
+module.exports = blogsRouter;
