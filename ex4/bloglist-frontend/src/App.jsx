e3767af3405dac31fbcbda86b526a95bf0cf2eb2
Peter Lee <76502486+peterlee42@users.noreply.github.com>
2025-07-15 21:01:51 -0400

    added login page to blog page

--- ex4/bloglist-frontend/src/App.jsx
+++ ex4/bloglist-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Blog from './components/Blog';
+import Notification from './components/Notification';
 import blogService from './services/blogs';
 import loginService from './services/login';
 
@@ -12,6 +13,9 @@ const App = () => {
 	const [title, setTitle] = useState('');
 	const [author, setAuthor] = useState('');
 	const [url, setUrl] = useState('');
+
+	const [message, setMessage] = useState('');
+	const [messageType, setMessageType] = useState('');
 
 	useEffect(() => {
 		const savedUser = JSON.parse(window.localStorage.getItem('blogAppUser'));
@@ -36,12 +40,24 @@ const App = () => {
 			setUser(user);
 			setUsername('');
 			setPassword('');
+			setMessage('logged in');
+			setMessageType('success');
 		} catch (err) {
 			console.log(err);
+			setMessage('wrong username or password');
+			setMessageType('error');
 		}
+
+		setTimeout(() => {
+			setMessage('');
+			setMessageType('');
+		}, 5000);
 	};
 
 	const handleLogout = () => {
+		setMessage('');
+		setMessageType('');
+
 		window.localStorage.removeItem('blogAppUser');
 		setUser(null);
 	};
@@ -49,21 +65,34 @@ const App = () => {
 	const handleCreate = async (e) => {
 		e.preventDefault();
 
-		const newBlog = {
-			author,
-			title,
-			url,
-		};
+		try {
+			const newBlog = {
+				author,
+				title,
+				url,
+			};
 
-		const savedBlog = await blogService.create(newBlog);
-		setBlogs(blogs.concat(savedBlog));
-		console.log(savedBlog);
+			const savedBlog = await blogService.create(newBlog);
+			setBlogs(blogs.concat(savedBlog));
+			console.log(savedBlog);
+			setMessage(`${savedBlog.title} by ${savedBlog.author} has been added`);
+			setMessageType('success');
+		} catch (err) {
+			setMessage('could not create a new blog');
+			setMessageType('error');
+		}
+
+		setTimeout(() => {
+			setMessage('');
+			setMessageType('');
+		}, 5000);
 	};
 
-	const loginForm = () => {
-		return (
+	const loginForm = () => (
+		<div>
+			<h2>log in to application</h2>
+			<Notification message={message} messageType={messageType} />
 			<form method='post' onSubmit={handleLogin}>
-				<h2>log in to application</h2>
 				<div>
 					username
 					<input
@@ -84,56 +113,55 @@ const App = () => {
 				</div>
 				<button type='submit'>login</button>
 			</form>
-		);
-	};
+		</div>
+	);
 
-	const blogsList = () => {
-		return (
-			<div>
-				<h2>blogs</h2>
-				<div style={{ display: 'flex', gap: '5px' }}>
-					<div>{user.name} is logged in</div>
-					<button onClick={handleLogout}>logout</button>
-				</div>
-				<form
-					onSubmit={handleCreate}
-					style={{ marginTop: '20px', marginBottom: '20px' }}
-				>
-					<div>
-						title:
-						<input
-							type='text'
-							name='Title'
-							value={title}
-							onChange={({ target }) => setTitle(target.value)}
-						/>
-					</div>
-					<div>
-						author:
-						<input
-							type='text'
-							name='Author'
-							value={author}
-							onChange={({ target }) => setAuthor(target.value)}
-						/>
-					</div>
-					<div>
-						url:
-						<input
-							type='text'
-							name='Url'
-							value={url}
-							onChange={({ target }) => setUrl(target.value)}
-						/>
-					</div>
-					<button type='submit'>create</button>
-				</form>
-				{blogs.map((blog) => (
-					<Blog key={blog.id} blog={blog} />
-				))}
+	const blogsList = () => (
+		<div>
+			<h2>blogs</h2>
+			<Notification message={message} messageType={messageType} />
+			<div style={{ display: 'flex', gap: '5px' }}>
+				<div>{user.name} is logged in</div>
+				<button onClick={handleLogout}>logout</button>
 			</div>
-		);
-	};
+			<form
+				onSubmit={handleCreate}
+				style={{ marginTop: '20px', marginBottom: '20px' }}
+			>
+				<div>
+					title:
+					<input
+						type='text'
+						name='Title'
+						value={title}
+						onChange={({ target }) => setTitle(target.value)}
+					/>
+				</div>
+				<div>
+					author:
+					<input
+						type='text'
+						name='Author'
+						value={author}
+						onChange={({ target }) => setAuthor(target.value)}
+					/>
+				</div>
+				<div>
+					url:
+					<input
+						type='text'
+						name='Url'
+						value={url}
+						onChange={({ target }) => setUrl(target.value)}
+					/>
+				</div>
+				<button type='submit'>create</button>
+			</form>
+			{blogs.map((blog) => (
+				<Blog key={blog.id} blog={blog} />
+			))}
+		</div>
+	);
 
 	return <div>{user === null ? loginForm() : blogsList()}</div>;
 };
---
+++ ex4/bloglist-frontend/src/components/Notification.jsx
@@ -0,0 +1,33 @@
+const Notification = ({ message, messageType }) => {
+	if (message === '') {
+		return;
+	}
+	const successStyle = {
+		color: '#03C04A',
+		border: 'solid 3px #03C04A',
+		backgroundColor: '#D3D3D3',
+		padding: '10px 5px 10px 5px',
+	};
+
+	const errorStyle = {
+		color: '#f01e2c',
+		border: 'solid 3px #f01e2c',
+		backgroundColor: '#D3D3D3',
+		padding: '10px 5px 10px 5px',
+	};
+
+	let notificationStyle = null;
+	if (messageType === 'success') {
+		notificationStyle = successStyle;
+	} else if (messageType === 'error') {
+		notificationStyle = errorStyle;
+	}
+
+	return (
+		<div>
+			<h3 style={notificationStyle}>{message}</h3>
+		</div>
+	);
+};
+
+export default Notification;
